8aa924a516d91be86175874d714d424b7d4902c2
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-25 23:56:15 +1000

    feat: adds animated background elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} relative bg-gray-50 text-zinc-950`}>
+        <div className="absolute -left-36 -top-36 blur-[6rem] rounded-full aspect-square bg-gradient-to-r from-green-500 to-purple-500 h-96 -z-10 overflow-x-hidden motion-safe:animate-spin-slow"></div>
+        <div className="absolute -right-14 -top-36 blur-[6rem] rounded-full aspect-square bg-gradient-to-r from-blue-500 to-red-500 h-72 -z-10 overflow-x-hidden motion-safe:animate-spin-slow"></div>
+        {children}
+      </body>
     </html>
   );
 }
