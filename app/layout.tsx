f7b247de52aad9526684dae7f669833763e0b5a0
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-25 23:20:32 +1000

    build: scaffolds the project

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ensar Okumus | Portfolio",
+  description:
+    "Check out my portfolio to see my full-stack development expertise and a collection of projects I'm currently working on.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
