76f2d40c4714caf7e0343d35a2a2b75d4f8ea991
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-28 17:47:22 +1000

    fix: overflow issue for background animations

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,20 +19,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-16 sm:pt-30 overflow-x-hidden`}
+        className={`${inter.className} bg-gray-50 text-gray-950 pt-16 sm:pt-30`}
       >
         <div
           aria-hidden
           className="absolute -z-40 inset-0 bg-[radial-gradient(#69696959_1px,transparent_1px)] bg-[size:34px_34px] opacity-80 motion-safe:animate-move-bg"
         />
-        <div
-          aria-hidden
-          className="absolute -left-36 -top-36 blur-[6rem] rounded-full aspect-square bg-gradient-to-r from-green-500 to-purple-500 h-96 -z-50 motion-safe:animate-spin-slow"
-        />
-        <div
-          aria-hidden
-          className="absolute -right-14 -top-36 blur-[6rem] rounded-full aspect-square bg-gradient-to-r from-blue-500 to-red-500 h-72 -z-50 motion-safe:animate-spin-slow"
-        ></div>
+        <div aria-hidden className="relative overflow-x-clip">
+          <div
+            aria-hidden
+            className="absolute -left-36 -top-36 blur-[6rem] rounded-full aspect-square bg-gradient-to-r from-green-500 to-purple-500 h-96 -z-50 motion-safe:animate-spin-slow"
+          />
+          <div
+            aria-hidden
+            className="absolute -right-14 -top-36 blur-[6rem] rounded-full aspect-square bg-gradient-to-r from-blue-500 to-red-500 h-72 -z-50 motion-safe:animate-spin-slow"
+          />
+        </div>
         <Navbar />
         {children}
       </body>
