45e468533798d7dbe7899464fa365b6a7cb06368
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-06-28 18:08:10 +1000

    fix: finalises styling of About section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/About";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center px-4">
       <About />
     </main>
   );
--- components/About.tsx
+++ components/About.tsx
@@ -35,7 +35,7 @@ const Letter: React.FC<LetterProps> = ({
 
 const About: React.FC = () => {
   return (
-    <section className="flex flex-col sm:flex-row justify-center items-center gap-10 lg:gap-40 px-4 lg:px-16 pt-16 overflow-x-hidden">
+    <section className="flex flex-col sm:flex-row justify-center items-center gap-5 lg:gap-40 lg:px-16 pt-16">
       <div className="relative flex flex-col items-center lg:items-start">
         <h1 className="text-6xl sm:text-8xl font-bold max-w-lg text-center lg:text-left text-gray-200">
           <span className="block">
@@ -74,12 +74,12 @@ const About: React.FC = () => {
               letter="."
               duration={2.2}
               delay={2.2}
-              className="text-orange-400"
+              className="text-blue-400"
             />
           </span>
         </h1>
 
-        <div className="flex items-center justify-center gap-5">
+        <div className="flex mt-6 items-center justify-center gap-5 flex-wrap">
           <motion.button
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -88,24 +88,25 @@ const About: React.FC = () => {
               delay: 2.2,
               ease: [0, 0.71, 0.2, 1.01],
             }}
-            className="mt-6 px-4 py-2 text-sm text-gray-600 border border-gray-600 rounded-lg hover:bg-gray-200 hover:text-gray-800 transition-colors duration-300"
+            className="px-4 py-2 border border-gray-300 bg-white border-opacity-40 shadow-xl shadow-black/[0.1] backdrop-blur-[0.5rem] text-sm text-gray-600 rounded-full hover:bg-gray-100 hover:text-gray-800 transition-colors duration-300"
             // TODO: add onClick to scroll to contact section
           >
             &lt;Contact Me /&gt;
           </motion.button>
-          <motion.button
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 0.8,
-              delay: 2.2,
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-            className="flex items-center gap-3 mt-6 px-4 py-2 text-sm text-gray-600 border border-gray-600 rounded-lg hover:bg-gray-200 hover:text-gray-800 transition-colors duration-300"
-            // TODO: add onClick to download resume
-          >
-            Download CV <FaDownload className="h-5 w-5" />
-          </motion.button>
+          <Link href="/Resume_EnsarOkumus_Jun24.pdf" download target="_blank">
+            <motion.button
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 2.2,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+              className="flex items-center gap-3 px-4 py-2 border border-gray-300 bg-white border-opacity-40 shadow-xl shadow-black/[0.1] backdrop-blur-[0.5rem] text-sm text-gray-600 rounded-full hover:bg-gray-100 hover:text-gray-800 transition-colors duration-300"
+            >
+              Download CV <FaDownload className="h-5 w-5" />
+            </motion.button>
+          </Link>
           <Link href="https://github.com/ensarokumus" target="_blank">
             <motion.button
               initial={{ opacity: 0, scale: 0.5 }}
@@ -115,7 +116,7 @@ const About: React.FC = () => {
                 delay: 2.2,
                 ease: [0, 0.71, 0.2, 1.01],
               }}
-              className="flex items-center gap-3 mt-6 px-4 py-2 text-sm text-gray-600 border border-gray-600 rounded-lg hover:bg-gray-200 hover:text-gray-800 transition-colors duration-300"
+              className="px-2 py-2 border border-gray-300 bg-white border-opacity-40 shadow-xl shadow-black/[0.1] backdrop-blur-[0.5rem] text-sm text-gray-600 rounded-full hover:bg-gray-100 hover:text-gray-800 transition-colors duration-300"
             >
               <FaGithub className="h-5 w-5" />
             </motion.button>
@@ -129,7 +130,7 @@ const About: React.FC = () => {
                 delay: 2.2,
                 ease: [0, 0.71, 0.2, 1.01],
               }}
-              className="flex items-center gap-3 mt-6 px-4 py-2 text-sm text-gray-600 border border-gray-600 rounded-lg hover:bg-gray-200 hover:text-gray-800 transition-colors duration-300"
+              className="px-2 py-2 border border-gray-300 bg-white border-opacity-40 shadow-xl shadow-black/[0.1] backdrop-blur-[0.5rem] text-sm text-gray-600 rounded-full hover:bg-gray-100 hover:text-gray-800 transition-colors duration-300"
             >
               <FaLinkedin className="h-5 w-5" />
             </motion.button>
@@ -147,35 +148,32 @@ const About: React.FC = () => {
           ease: [0, 0.71, 0.2, 1.01],
         }}
       >
-        <Image
-          src="/ensar-profile-pic.png"
-          alt="Ensar Okumus"
-          height={175}
-          width={175}
-          fetchPriority="high"
-          className="grayscale rounded-xl drop-shadow-xl"
-        />
-        <p className="mt-6 text-gray-600 text-base max-w-lg border-gray-700 pl-4">
-          Hello! I&apos;m Ensar, a dedicated{" "}
-          <span className="font-medium text-orange-400">
-            full-stack developer
+        <div className="relative">
+          <Image
+            src="/ensar-profile-pic.png"
+            alt="Ensar Okumus"
+            height={225}
+            width={225}
+            priority
+            className="relative z-20 rounded-xl drop-shadow-xl grayscale"
+          />
+          <div
+            aria-hidden
+            className="absolute z-10 top-[15px] right-[20px] w-full h-[80%] rounded-xl border-gray-400 border-4"
+          ></div>
+          <div
+            aria-hidden
+            className="absolute bottom-0 w-full h-[80%] place-self-end rounded-xl bg-gradient-to-r from-green-400/50 via-blue-400/50 to-red-400/50"
+          ></div>
+        </div>
+
+        <p className="mt-6 text-gray-600 text-lg max-w-lg text-center sm:text-left">
+          Hello! I&apos;m Ensar, a{" "}
+          <span className="font-medium text-blue-400">
+            full stack developer
           </span>{" "}
           with a focus on{" "}
-          <span className="font-medium text-orange-400">ReactJS.</span> Welcome
-          to my portfolio, where you can explore my work on{" "}
-          <span className="font-medium text-orange-400">
-            responsive web applications,
-          </span>{" "}
-          creating{" "}
-          <span className="font-medium text-orange-400">
-            intuitive user interfaces,
-          </span>{" "}
-          and{" "}
-          <span className="font-medium text-orange-400">
-            optimising website performance.
-          </span>
-          <br />
-          <br />
+          <span className="font-medium text-blue-400">ReactJS. </span>
           I&apos;m excited to share my journey and projects with you. Enjoy your
           visit!
         </p>
