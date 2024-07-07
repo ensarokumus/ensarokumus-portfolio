d366952d5e61ce2c7b73a944c97c27e86388dacc
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-07-07 17:00:09 +1000

    feat: updates About section

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -13,24 +13,32 @@ export default function About() {
       <SectionHeading>about me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-semibold">Industrial Engineering</span>, I had
-        over a decade of experience in multi-national companies in marketing,
-        data analytics and graphic design. Designing for digital mediums helped
-        me to discover my passion for programming and I decided to go on a
-        self-learning journey for becoming a{" "}
-        <span className="font-semibold">full-stack web developer</span>.{" "}
+        <span className="font-semibold dark:text-orange-500">
+          Industrial Engineering
+        </span>
+        , I had over a decade of experience in multi-national companies in
+        marketing, data analytics and graphic design. Designing for digital
+        mediums helped me to discover my passion for programming and I decided
+        to go on a self-learning journey for becoming a{" "}
+        <span className="font-semibold dark:text-orange-500">
+          full-stack web developer
+        </span>
+        .{" "}
       </p>
 
       <p>
         <em>My favorite part of programming</em> is the problem-solving aspect.
         I <em>love</em> the feeling of finally figuring out a solution to a
         problem. My core stack is{" "}
-        <span className="font-semibold">
+        <span className="font-semibold dark:text-orange-500">
           React, TypeScript, Next.js, Node.js, Express and MongoDB
         </span>
         . I am always looking to learn new technologies and currently looking
-        for a <span className="font-semibold">full-time position</span> as a
-        software developer.
+        for a{" "}
+        <span className="font-semibold dark:text-orange-500">
+          full-time position
+        </span>{" "}
+        as a software developer.
       </p>
     </motion.section>
   );
